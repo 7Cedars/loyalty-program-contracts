f7c703c65242fe0f8ec4406864e8087a2390f6ff
Seven Cedars <Cedars7@proton.me>
2024-05-14 14:33:25 +0100

    modules

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -1,5 +1,5 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.24;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
 
 // // based on: Patrick Collins: helperConfig.s.sol + learning/foundry-fund-me-f23
 
